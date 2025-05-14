981f220bb294c24266f308c3c8c92fc23b40c471
advait-ravi-marathe <ady2112mar@gmail.com>
2025-05-14 08:14:10 +0530

    Modified folder structure

---
+++ learn-spring-02/src/main/java/com/courseIn28Mins/learn_spring/game_spring/GameRetrieval.java
@@ -0,0 +1,19 @@
+package com.courseIn28Mins.learn_spring.game_spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class GameRetrieval {
+
+    public static void main(String[] args) {
+
+        //1. Launch a spring context
+        try (var context = new AnnotationConfigApplicationContext(GameConfiguration.class)) {
+
+            //2. Retrieving bean's up method
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+
+        }
+
+    }
+}
