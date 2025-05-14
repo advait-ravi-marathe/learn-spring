851d15faeb03474b412f6c59fe0a4d98d83aa97c
advait-ravi-marathe <ady2112mar@gmail.com>
2025-05-14 08:31:29 +0530

    Optimized code

--- src/main/java/com/courseIn28Mins/learn_spring/game/GameRetrieval.java
+++ src/main/java/com/courseIn28Mins/learn_spring/game/GameRetrieval.java
@@ -1,16 +1,33 @@
 package com.courseIn28Mins.learn_spring.game;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//Configure the things that we want Spring to manage
+@Configuration
 public class GameRetrieval {
+
+    //Step 1: Creation of object
+    @Bean
+    public GamingConsole game() {
+        return new PacManGame();
+    }
+
+    //Retrieves bean named game and passes that as a parameter
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
 
     public static void main(String[] args) {
 
         //1. Launch a spring context
-        try (var context = new AnnotationConfigApplicationContext(GameConfiguration.class)) {
+        try (var context = new AnnotationConfigApplicationContext(GameRetrieval.class)) {
 
             //2. Retrieving bean's up method
             context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
 
         }
 
